4f8c44c7b59803a78f8bba57d4133d9fce46c1d6
Lokeswara Reddy <166979268+techabbayi@users.noreply.github.com>
2026-05-12 14:06:25 +0530

    Add solution for Permutations II

--- LeetCode/Permutations-II/Solution.py
+++ LeetCode/Permutations-II/Solution.py
@@ -1,33 +1,21 @@
-1class Solution(object):
-2    def permuteUnique(self, nums):
-3        """
-4        :type nums: List[int]
-5        :rtype: List[List[int]]
-6        """
-7        result = []
-8
-9        def backtrack(path, remaining):
-10
-11            # permutation completed
-12            if not remaining:
-13                result.append(path)
-14                return
-15
-16            used = set()
-17
-18            for i in range(len(remaining)):
-19
-20                # skip duplicates
-21                if remaining[i] in used:
-22                    continue
-23
-24                used.add(remaining[i])
-25
-26                backtrack(
-27                    path + [remaining[i]],
-28                    remaining[:i] + remaining[i+1:]
-29                )
-30
-31        backtrack([], nums)
-32
-33        return result        
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        results = []
+        def bt(start):
+            if start == len(nums):
+                results.append(nums[:])
+                return
+            
+            
+            lookup = set()
+            
+            for i in range(start,len(nums)):
+                if nums[i] not in lookup:
+                    nums[start], nums[i]=  nums[i],nums[start]
+                    bt(start+1)
+                    nums[start], nums[i]=  nums[i],nums[start]
+                    lookup.add(nums[i])
+        
+        bt(0)
+        return results
